7ea14da58d6d9371e7b3239ef5693787757a4cc0
Andrei Savaskov <ultrazokk@gmail.com>
2024-04-27 11:52:12 +0300

    создание класса ToyRaffle, который управляет процессом розыгрыша

--- ToyRaffle.java
+++ ToyRaffle.java
@@ -9,6 +9,21 @@ public class ToyRaffle {
     private PriorityQueue<Toy> toys;
     private Random random;
 
-    
+    public ToyRaffle() {
+        this.toys = new PriorityQueue<>(Comparator.comparingInt(Toy::getWeight))
+        this.random = new Random();
+    }
+
+    public void addToy(Toy toy) {
+        toys.add(toy);
+    }    
+
+    public Toy draw() {
+        Toy toy = toys.poll();
+        if (toy != null) {
+            toys.add(toy);
+        }
+        return toy;
+    }
     
 }
